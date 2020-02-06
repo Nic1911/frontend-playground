f0cb6c6bc45a5ef07f8df1e8c1b059dc5db5ab9d
Nic1911 <nicolo.simioni.cfv@gmail.com>
2020-02-06 20:12:14 +0100

    init

---
+++ src/scripts/script.js
@@ -0,0 +1,96 @@
+console.log(
+    '%c Coded by: Nicolò Simioni',
+    'background: darkgoldenrod; color: #fff; border: 1px solid #000; padding: 4px; padding-top: 10px; padding-bottom: 8px;'
+);
+console.log(
+    '%c This webpack boilerplate has been developed by Tristan Lawrence => https://twitter.com/triscodes',
+    'background: lavenderblush; border: 1px solid #000; padding: 4px; padding-top: 10px; padding-bottom: 8px;'
+);
+
+import {
+    loaderAnim,
+    animaNow,
+    moveTitle,
+    resetMoveTitle,
+    randomRed
+} from "./animation.js";
+import {
+    initScrollMagicInnovation
+} from "./innovation.js";
+import {
+    pressingDown,
+    notPressingDown,
+    doSomething,
+    responsiveBackgroundIMage,
+    detectElementsInDOM,
+    elemInViewport,
+    splitWords,
+    splitWordsInner,
+    initCarousel
+} from "./utils.js";
+
+
+
+const initClickHold = () => {
+    const item = document.querySelector('#item');
+    const movingElems = document.getElementsByClassName('ns-moveOnHover');
+    for (let index = 0; index < movingElems.length; index++) {
+        //const element = array[index];
+        movingElems[index].addEventListener('mousemove', moveTitle, false);
+        movingElems[index].addEventListener('mouseleave', resetMoveTitle, false);
+    }
+
+
+    // Listening for the mouse and touch events    
+    item.addEventListener('mousedown', pressingDown, false);
+    item.addEventListener('mouseup', notPressingDown, false);
+    item.addEventListener('mouseleave', notPressingDown, false);
+
+    item.addEventListener('touchstart', pressingDown, false);
+    item.addEventListener('touchend', notPressingDown, false);
+
+    // Listening for our custom pressHold event
+    item.addEventListener('pressHold', doSomething, false);
+}
+
+
+window.addEventListener("resize", detectElementsInDOM, false);
+window.addEventListener("scroll", detectElementsInDOM, false);
+
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+
+window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            animaNow(last_known_scroll_position);
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
+
+
+const init = () => {
+    initClickHold();
+    const global = new TimelineMax();
+    global // global Timeline
+        .add(loaderAnim)
+
+    splitWords();
+    initScrollMagicInnovation();
+    randomRed();
+}
+
+
+window.addEventListener('load', function() {
+
+    init();
+    responsiveBackgroundIMage();
+    initCarousel();
+})
